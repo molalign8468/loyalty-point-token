c02df3432e0c0517da04d1ca45de7c51f2cacd3c
molalign8468 <molaligngetahun229@gmail.com>
2025-08-20 00:55:09 +0300

    feat: Configure Hardhat for Holesky deployment

--- hardhat.config.cjs
+++ hardhat.config.cjs
@@ -1,6 +1,17 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.28",
+  networks: {
+    holesky: {
+      url: process.env.ALCHEMY_HOLESKY_URL || "",
+      accounts: process.env.PRIVATE_KEY ? [`0x${process.env.PRIVATE_KEY}`] : [],
+      chainId: 17000,
+    },
+  },
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_API_KEY,
+  },
 };
